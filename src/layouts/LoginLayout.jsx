8bbb4cfbe94913a0baf43d208b18dbc8fe2926c2
Buddynice34 <willemapril33@gmail.com>
2026-01-11 20:55:23 +0200

    Adjust layout classes for login components
    Removed fixed width and height classes from LoginLayout's main element and updated LoginPage's container to use h-screen instead of md:h-screen for improved layout consistency.

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -3,7 +3,7 @@ import LoginPage from '../pages/LoginPage'
 
 function LoginLayout() {
   return (
-      <main className='w-screen h-screen flex items-center justify-center'>
+      <main className=''>
         <LoginPage/>
     </main>
   )
