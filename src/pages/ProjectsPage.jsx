bc51903e5a92e7ce4569c52d3173626cbe896648
Buddynice34 <willemapril33@gmail.com>
2026-01-11 22:12:03 +0200

    Enhance ProjectsPage with header and table
    Added a styled header, an 'Add Project' button with icon, and integrated the ProjectsTable component to improve the ProjectsPage layout and functionality.

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { FaPlus, FaSearch } from "react-icons/fa";
+import ProjectsTable from "../components/ProjectsTable";
 
 function ProjectsPage() {
   return (
-    <div>ProjectsPage</div>
+    <section>
+      {/* navigation top section*/}
+      <div className="flex items-center justify-between mb-8 px-4">
+        <h2 className="font-bold leading-5 tracking-tight text-xl md:text-2xl lg:text-4xl text-gray-900">Projects</h2>
+        <button className="flex items-center px-3 py-3 shadow-xs rounded-lg bg-blue-500 text-white font-base hover:bg-blue-600 tracking-tight active:scale-95 transition duration-75 cursor-pointer"><FaPlus className="h-4 w-4 me-2"/> Add Project</button>
+      </div>
+
+      <ProjectsTable/>
+    </section>
   )
 }
 
